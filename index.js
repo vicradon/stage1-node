05654e0a0c5dc8767521f913626c65e2adffb32b
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2023-09-09 11:34:06 +0000

    update code

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+require("dotenv").config();
+const app = express();
 
-app.get('/info', (req, res) => {
-  const slack_name = req.query.slack_name
-  const current_day = 'Saturday'
-  const track = req.query.track
-  const utcTime = new Date(Date.now()).toISOString().split('.')[0] + 'Z'
-  const github_file_url = 'https://github.com/bintus-ux/stage-one-main-source'
+const PORT = process.env.PORT || 5000;
+
+app.get("/api", (req, res) => {
+  const slack_name = req.query.slack_name;
+  const current_day = "Saturday";
+  const track = req.query.track;
+  const utcTime = new Date(Date.now()).toISOString().split(".")[0] + "Z";
+  const github_file_url =
+    "https://github.com/bintus-ux/stage-one-general-source/blob/main/index.js";
   const github_repo_url =
-    'https://github.com/bintus-ux/stage-one-general-source'
-  const status_code = 200
+    "https://github.com/bintus-ux/stage-one-general-source";
+  const status_code = 200;
 
   const info = {
     slack_name: slack_name,
     current_day: current_day,
-    utc_Time: utcTime,
+    utc_time: utcTime,
     track: track,
     github_file_url: github_file_url,
     github_repo_url: github_repo_url,
     status_code: status_code,
-  }
+  };
 
-  res.json(info)
-})
-// app.get('/', (req, res) => {
-//   res.send('API is running....')
-// })
+  res.json(info);
+});
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000')
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
